a8a0f5ce085d8831fe3f69abc1a7544aa7db367c
STONE17th <17th@inbox.ru>
2022-08-03 07:28:36 +0300

    Утренний пре-релиз

--- data_ship.py
+++ data_ship.py
@@ -2,90 +2,36 @@ import random
 from tkinter import messagebox
 
 
+def create_dictionary():
+    dict = {}
+    index = 1
+    for i in range(1,11):
+        for j in range(1,11):
+            dict[index] = (i,j)
+            index += 1
+    return dict
 
-def random_ship_position(player_ships, dictionary):
-
+def random_ship_position(player_ships):
     ships_position = []
-    ships_position.clear()
     buffer_position = []
-    buffer_position.clear()
     counter = 0
 
-    def xy_to_id(ship):
-        k = [k for k, v in dictionary.items() if v == (ship.y, ship.x)]
-        if not k == []: return int(k[0])
-        else: return 0
-
-    def cell_ship(ship):
-        temp_ship = []
-        x, y = ship.x, ship.y
-        if ship.size == 1:
-            temp_ship.append(int(xy_to_id(ship)))
-            ship.x, ship.y = x, y
-            return temp_ship
-        else:
-            for i in range(ship.size):
-                temp_ship.append(xy_to_id(ship))
-                if ship.direct:
-                    ship.y += 1
-                else:
-                    ship.x += 1
-            ship.x, ship.y = x, y
-            return temp_ship
-
-    def cell_buffer(ship):
-        temp_buffer = []
-        buffer = [-1, 0, 1]
-        x, y = ship.x, ship.y
-        if ship.size == 1:
-            for i in buffer:
-                for j in buffer:
-                    temp_x, temp_y = ship.x, ship.y
-                    ship.x += i
-                    ship.y += j
-                    temp_buffer.append(xy_to_id(ship))
-                    ship.x, ship.y = temp_x, temp_y
-        else:
-            for i in range(ship.size):
-                for i in buffer:
-                    for j in buffer:
-                        temp_x, temp_y = ship.x, ship.y
-                        ship.x += i
-                        ship.y += j
-                        temp_buffer.append(xy_to_id(ship))
-                        ship.x, ship.y = temp_x, temp_y
-                if ship.direct:
-                    ship.y += 1
-                else:
-                    ship.x += 1
-        for cell in temp_ship:
-            if cell in temp_buffer: temp_buffer.remove(cell)
-        ship.x, ship.y = x, y
-        return set(temp_buffer)
-
     def create_ship(ship):
-        ship.x = random.randint(1, 11)
-        ship.y = random.randint(1, 11)
         if random.randint(0, 1):
             ship_rotate_random(ship)
-            if ship.x < 11 and ship.size + ship.y < 11:
-                return True
-        else:
-            if ship.x + ship.x < 11 and ship.y < 11:
-                return True
-
-    def comparison(ship, list):
-        for cell in ship:
-            if cell in list:
+        ship.x = random.randint(1, 10)
+        ship.y = random.randint(1, 10)
+        for cell in ship.cell_ship():
+            if not 0<cell<101:
                 return False
-        else: return True
+        return True
 
     for ship in player_ships:
         while True:
             if counter > 10000: break
             if create_ship(ship):
-                temp_ship = cell_ship(ship)
-                temp_buffer = cell_buffer(ship)
+                temp_ship = ship.cell_ship()
+                temp_buffer = ship.cell_buffer()
                 if comparison(temp_ship, ships_position):
                     if comparison(temp_ship, buffer_position):
                         [ships_position.append(temp) for temp in temp_ship]
@@ -93,9 +39,18 @@ def random_ship_position(player_ships, dictionary):
                         ship.place(x=(ship.x-1) * 30 + 10, y=(ship.y - 1) * 30 + 10)
                         break
             counter += 1
-    if len(ships_position) == 20: return ships_position
-    else: random_ship_position(player_ships)
 
+
+def comparison(ship, list):
+        for cell in ship:
+            if cell in list:
+                return False
+        else: return True
+
+def xy_to_id(ship):
+    k = [k for k, v in dict_id.items() if v == (ship.y, ship.x)]
+    if not k == []: return int(k[0])
+    else: return 0
 
 def ship_rotate_random(ship):
     ship.change_dir()
@@ -117,3 +72,33 @@ def out_of_shipyard(ship_position):
                 messagebox.showinfo('Капитан!', 'Капитан, мы не можем отправить расположение кораблей! Не все корабли в зоне действий!')
                 return False
     return True
+
+def ship_collision(list, message):
+    buffer_zone = []
+    for ship in list:
+        for cell in ship.cell_buffer():
+            if not cell in buffer_zone: buffer_zone.append(cell)
+    for ship in list:
+        for cell in ship.cell_ship():
+            if cell in buffer_zone:
+                if message:
+                    ship['bg'] = 'RED'
+                    messagebox.showinfo('Капитан!', 'Капитан, бой еще не начался, а мы уже врезались! Расставьте корабли на расстоянии хотя бы ОДНОЙ клетки!')
+                return True
+    return False
+
+def ship_overlay(list, message):
+    for ship_on_water in list:
+        for ship in list:
+            for cell in ship.cell_ship():
+                if cell in ship_on_water.cell_ship() and not ship.id == ship_on_water.id:
+                    if message:
+                        ship_on_water['bg'] = 'RED'
+                        ship['bg'] = 'RED'
+                        messagebox.showinfo('Капитан!', 'Капитан, не мне вас учить, но корабль не может быть в КОРАБЛЕ! Поставьте рядышком, что ли...')
+                    return True
+    return False
+
+
+
+dict_id = create_dictionary()
